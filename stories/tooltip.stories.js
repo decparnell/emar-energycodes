ffb039725ee5ad3d810a62d2d22f7a7e12fd07e9
Mavuru <princess.mavuru@capgemini.com>
2022-07-21 17:54:18 +0100

    updated css modules

---
+++ stories/tooltip.stories.js
@@ -0,0 +1,11 @@
+import { useRouter } from "next/router";
+import React, { Component } from 'react'; 
+
+import {toolTip} from "../components/tooltip/tooltip.js"
+import '../../styles/tooltip.module.css'
+
+
+export default {
+    title: 'toolTip',
+    component: toolTip, 
+};
