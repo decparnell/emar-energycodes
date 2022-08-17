da342f51a61300d1cfc078274f1e97961109f600
Parnell <dec.parnell@capgemini.com>
2022-08-17 09:38:03 +0100

    spelling mistake fixed

--- pages/_app.js
+++ pages/_app.js
@@ -6,9 +6,9 @@ import { useState } from "react";
 
 export default function MyApp({ Component, pageProps }) {
   const [allDataSpecVersions, setAllDataSpecVersions] = useState([
-    { versionNumber: "2.2.1", status: "Superseeded" },
-    { versionNumber: "2.2.2", status: "Superseeded" },
-    { versionNumber: "2.3.0", status: "Superseeded" },
+    { versionNumber: "2.2.1", status: "Superseded" },
+    { versionNumber: "2.2.2", status: "Superseded" },
+    { versionNumber: "2.3.0", status: "Superseded" },
     { versionNumber: "3.0.0", status: "Live" },
     { versionNumber: "7.0.0", status: "Future" },
   ]); //
