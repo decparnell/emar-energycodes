c2f72e913bedb8eb0fff8a17f00e4a5a6426f8f1
Parnell <dec.parnell@capgemini.com>
2022-10-04 07:25:00 +0100

    adding schedule links to thedefinitions

--- components/helperFunctions/onHoverDefinition/index.js
+++ components/helperFunctions/onHoverDefinition/index.js
@@ -1,11 +1,15 @@
-import React, { useState } from "react";
 import styles from "../../../styles/helperFunctions.module.css";
+import React, { useState, useContext } from "react";
+import AppContext from "../../context/AppContext";
 
 function HoverOverFunctionDefinition(
   definitionString,
+  linkType,
   linkingWord,
   linkAddress
 ) {
+  const value = useContext(AppContext);
+  let { latestDataSpecVersion } = value.state;
   const [isShown, setIsShown] = useState(false);
   const [delayHandler, setDelayHandler] = useState(null);
   const handleMouseEnter = (event) => {
@@ -23,17 +27,42 @@ function HoverOverFunctionDefinition(
 
   return (
     <>
-      <a
-        className={styles.tooltiptext}
-        href={`/codes-schedules/definitions/${encodeURIComponent(linkAddress)}`}
-        target="_blank"
-        rel="noreferrer"
-        onMouseEnter={handleMouseEnter}
-        onMouseLeave={handleMouseLeave}
-      >
-        {linkingWord}
-      </a>
-      {isShown && <div className={styles.tooltip}>{definitionString}</div>}
+      {linkType == "definition" ? (
+        <a
+          className={styles.tooltiptext}
+          href={`/codes-schedules/definitions/${encodeURIComponent(
+            linkAddress
+          )}`}
+          target="_blank"
+          rel="noreferrer"
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        >
+          {linkingWord}
+        </a>
+      ) : (
+        //CHANGE : Latest dataspec version for the version of the document
+        <a
+          className={styles.tooltiptext}
+          href={`/codes-schedules/${encodeURIComponent(
+            linkAddress
+          )}/${encodeURIComponent(latestDataSpecVersion)}
+          `}
+          target="_blank"
+          rel="noreferrer"
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        >
+          {linkingWord}
+        </a>
+      )}
+      {isShown && (
+        <div className={styles.tooltip}>
+          {linkType == "definition"
+            ? definitionString
+            : "Click to go to the Schedule"}
+        </div>
+      )}
     </>
   );
 }
