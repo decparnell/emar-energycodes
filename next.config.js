df5d44d69e02b50db9531b43944f10424bc42d1e
Parnell <dec.parnell@capgemini.com>
2022-10-04 15:41:21 +0100

    adding longer page generation

--- next.config.js
+++ next.config.js
@@ -3,8 +3,11 @@ const nextConfig = {
   reactStrictMode: true,
 };
 
-module.exports = {images: {
-  domains: ['reccodocstroage.blob.core.windows.net'],
-}} 
-
-
+module.exports = {
+  images: {
+    domains: ["reccodocstroage.blob.core.windows.net"],
+    // time in seconds of no pages generating during static
+    // generation before timing out
+    staticPageGenerationTimeout: 1000,
+  },
+};
