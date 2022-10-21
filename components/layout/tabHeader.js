8279759956090acc16dd23d9278b633d4bf42ba9
Parnell <dec.parnell@capgemini.com>
2022-10-21 11:17:06 +0100

    changing search history css

--- components/layout/tabHeader.js
+++ components/layout/tabHeader.js
@@ -1,22 +1,8 @@
 import AppContext from "../context/AppContext";
 import { useContext } from "react";
 import styles from "../../styles/header.module.css";
-
+import { dashboards } from "../settings";
 function TabNavbar() {
-  const dashboards = [
-    {
-      dashboardId: 1,
-      dashboardName: "Codes Schedules",
-      dashboardColumns: 2,
-      dashboardOrder: 1,
-    },
-    {
-      dashboardId: 2,
-      dashboardName: "Data Specification",
-      dashboardColumns: 2,
-      dashboardOrder: 2,
-    },
-  ];
   const value = useContext(AppContext);
   let { chosenTab } = value.state;
 
