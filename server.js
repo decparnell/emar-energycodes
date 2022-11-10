f5765c62635214f0aee98ff78b389a816c2e6ed7
Parnell <dec.parnell@capgemini.com>
2022-11-10 18:39:08 +0000

    added user into saml

--- server.js
+++ server.js
@@ -67,9 +67,9 @@ app.prepare().then(() => {
 
       // Save name_id and session_index for logout
       // Note:  In practice these should be saved in the user session, not globally.
-      displayName = saml_response.DisplayName;
-      emailAddress = saml_response.email;
-      objectId = saml_response.NameID;
+      displayName = saml_response.user.DisplayName;
+      emailAddress = saml_response.user.email;
+      objectId = saml_response.user.NameID;
       res.send(
         "Hello #{displayName}! email: #{emailAddress} objectId: #{objectId}."
       );
