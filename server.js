6a498df6d9016583f704346e26a6b3e7e28c36ac
Parnell <dec.parnell@capgemini.com>
2022-11-14 19:25:04 +0000

    moving redirect

--- server.js
+++ server.js
@@ -75,7 +75,6 @@ app.prepare().then(() => {
         `https://prod-12.uksouth.logic.azure.com/workflows/a01770cba8f44c8a90274a6faa24955d/triggers/manual/paths/invoke/email/${name_id}?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=vm5xuq9xqyj6xN0P_NBrRPjDsElEJhOsWIWcmjfdzak`,
         function (error, response, body) {
           if (!error && response.statusCode == 200) {
-            res.redirect("/");
           } else {
             console.log(
               "Insert into Db Error ------ " +
@@ -87,6 +86,7 @@ app.prepare().then(() => {
         }
       );
     });
+    res.redirect("/");
   });
 
   // Starting point for logout
