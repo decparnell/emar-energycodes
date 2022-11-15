55d44e1352d9531e0377cda9dde3339807a28e8d
Parnell <dec.parnell@capgemini.com>
2022-11-15 11:30:03 +0000

    removing logging and api call

--- server.js
+++ server.js
@@ -77,8 +77,8 @@ app.prepare().then(() => {
       name_id = saml_response.user.name_id;
       session_index = saml_response.user.session_index;
     });
-
-    const https = require("https");
+    res.redirect("/");
+    /* const https = require("https");
     console.log("Email ------ " + userEmail);
     // Sample URL
     const url = `https://prod-12.uksouth.logic.azure.com/workflows/a01770cba8f44c8a90274a6faa24955d/triggers/manual/paths/invoke/email/${userEmail}?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=vm5xuq9xqyj6xN0P_NBrRPjDsElEJhOsWIWcmjfdzak`;
@@ -96,7 +96,7 @@ app.prepare().then(() => {
             " ------- " +
             error
         );
-      });
+      }); */
   });
 
   // Starting point for logout
