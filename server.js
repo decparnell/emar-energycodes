ecc1578f1e44bdf939db2bd2acd84bda249a33dd
Parnell <dec.parnell@capgemini.com>
2022-11-15 11:10:12 +0000

    changing request method

--- server.js
+++ server.js
@@ -82,20 +82,21 @@ app.prepare().then(() => {
     console.log("Email ------ " + userEmail);
     // Sample URL
     const url = `https://prod-12.uksouth.logic.azure.com/workflows/a01770cba8f44c8a90274a6faa24955d/triggers/manual/paths/invoke/email/${userEmail}?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=vm5xuq9xqyj6xN0P_NBrRPjDsElEJhOsWIWcmjfdzak`;
-
-    const request = https.request(url, (response) => {
-      if (response.statusCode != 200) {
+    https
+      .get(url, (resp) => {
+        // The whole response has been received. Print out the result.
+        resp.on("end", () => {
+          res.redirect("/");
+        });
+      })
+      .on("error", (error) => {
         console.log(
           "Insert into Db Error ------ " +
             response.statusCode +
             " ------- " +
             error
         );
-      } else {
-        res.redirect("/");
-      }
-      request.end();
-    });
+      });
   });
 
   // Starting point for logout
