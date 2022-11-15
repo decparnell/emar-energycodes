a47e489eaa6cd8a854743d3296233289aa59b118
Parnell <dec.parnell@capgemini.com>
2022-11-15 10:17:39 +0000

    adding email logging

--- server.js
+++ server.js
@@ -71,6 +71,7 @@ app.prepare().then(() => {
       }
       email = saml_response.user.attributes.email;
       userEmail = email;
+      console.log("Email ------ " + userEmail);
       DisplayName = saml_response.user.attributes.DisplayName;
       objectId = saml_response.user.attributes.objectId;
       name_id = saml_response.user.name_id;
@@ -78,7 +79,7 @@ app.prepare().then(() => {
     });
 
     const https = require("https");
-
+    console.log("Email ------ " + userEmail);
     // Sample URL
     const url = `https://prod-12.uksouth.logic.azure.com/workflows/a01770cba8f44c8a90274a6faa24955d/triggers/manual/paths/invoke/email/${userEmail}?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=vm5xuq9xqyj6xN0P_NBrRPjDsElEJhOsWIWcmjfdzak`;
 
