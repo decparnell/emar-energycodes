46a106facaf90efeef6e09c58655a7cb92637372
Parnell <dec.parnell@capgemini.com>
2022-11-29 09:59:26 +0000

    login prod

--- server.js
+++ server.js
@@ -154,9 +154,8 @@ app.prepare().then(() => {
   });
 
   server.all("*", (req, res) => {
-    //if (name_id) return handle(req, res);
-    //res.redirect("/login");
-    return handle(req, res);
+    if (name_id) return handle(req, res);
+    res.redirect("/login");
   });
 
   server.listen(port, () => {
