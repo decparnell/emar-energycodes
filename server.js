e6b684d096821fd6bf8f24211c1453176a9f64e6
Parnell <dec.parnell@capgemini.com>
2023-03-22 08:41:37 +0000

    line 185 changed to not equals

--- server.js
+++ server.js
@@ -182,7 +182,7 @@ app.prepare().then(() => {
   });
 
   server.all("*", (req, res) => {
-    if (req.session.user === undefined) return handle(req, res);
+    if (req.session.user !== undefined) return handle(req, res);
     res.redirect("/login");
     //return handle(req, res);
   });
