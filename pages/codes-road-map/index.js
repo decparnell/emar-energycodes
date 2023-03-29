363f7127a8c8b4f341a445669fd45a3cf7808210
Parnell <dec.parnell@capgemini.com>
2023-03-29 13:43:41 +0100

    fixes to codes road mao

--- pages/codes-road-map/index.js
+++ pages/codes-road-map/index.js
@@ -1,14 +1,12 @@
 import styles from "../../styles/codesRoadMap.module.css";
 import React from "react";
 
-function CodesRoadMap({
-  latestCodesRoadMapLinkJSON
-}) {
-
+function CodesRoadMap({ latestCodesRoadMapLinkJSON }) {
   const apiResList = [
-    { obj: latestCodesRoadMapLinkJSON, name: "latestCodesRoadMapLinkJSON" }
-  ]
-  const wikiPage = "https://recportal.co.uk/rec-wiki-landing/-/knowledge_base_search/677762514/maximized?_com_liferay_knowledge_base_web_portlet_SearchPortlet_redirect=https%3A%2F%2Frecportal.co.uk%3A443%2Frec-wiki-landing%3Fp_p_id%3Dcom_liferay_knowledge_base_web_portlet_SearchPortlet%26p_p_lifecycle%3D0%26p_p_state%3Dmaximized%26p_p_mode%3Dview%26_com_liferay_knowledge_base_web_portlet_SearchPortlet_mvcPath%3D%252Fsearch%252Fsearch.jsp%26_com_liferay_knowledge_base_web_portlet_SearchPortlet_keywords%3Droadmap%26_com_liferay_knowledge_base_web_portlet_SearchPortlet_formDate%3D1680000364694"
+    { obj: latestCodesRoadMapLinkJSON, name: "latestCodesRoadMapLinkJSON" },
+  ];
+  const wikiPage =
+    "https://recportal.co.uk/rec-wiki-landing/-/knowledge_base_search/677762514/maximized?_com_liferay_knowledge_base_web_portlet_SearchPortlet_redirect=https%3A%2F%2Frecportal.co.uk%3A443%2Frec-wiki-landing%3Fp_p_id%3Dcom_liferay_knowledge_base_web_portlet_SearchPortlet%26p_p_lifecycle%3D0%26p_p_state%3Dmaximized%26p_p_mode%3Dview%26_com_liferay_knowledge_base_web_portlet_SearchPortlet_mvcPath%3D%252Fsearch%252Fsearch.jsp%26_com_liferay_knowledge_base_web_portlet_SearchPortlet_keywords%3Droadmap%26_com_liferay_knowledge_base_web_portlet_SearchPortlet_formDate%3D1680000364694";
   return (
     <>
       <div className={styles.container}>
@@ -16,20 +14,65 @@ function CodesRoadMap({
           <tbody>
             <tr>
               <td>
-                <b>The REC Code Roadmap</b><br></br>
-                <p>The REC Code Roadmap provides a view of the key areas of strategic change that RECCo and the REC Code Manager will focus on over the next three years.  These are areas of the REC where significant benefit could be gained for all REC stakeholders including consumers, within the long-term energy retail landscape.</p>
-                <p>The ‘epics’ are the work items that will deliver the change.  The roadmap presents the epics that are active now and those that are due to be initiated in the coming months, so you can see what may be of interest to you and how you can get involved in shaping the solutions.</p>
+                <b>The REC Code Roadmap</b>
+                <br></br>
+                <p>
+                  The REC Code Roadmap provides a view of the key areas of
+                  strategic change that RECCo and the REC Code Manager will
+                  focus on over the next three years. These are areas of the REC
+                  where significant benefit could be gained for all REC
+                  stakeholders including consumers, within the long-term energy
+                  retail landscape.
+                </p>
+                <p>
+                  The ‘epics’ are the work items that will deliver the change.
+                  The roadmap presents the epics that are active now and those
+                  that are due to be initiated in the coming months, so you can
+                  see what may be of interest to you and how you can get
+                  involved in shaping the solutions.
+                </p>
               </td>
             </tr>
             <tr>
-              <td>{latestCodesRoadMapLinkJSON.map(item => <a href={"https://" + item.link} target="_blank">Spreadsheet to download containing the roadmap</a>)}</td>
+              <td>
+                {latestCodesRoadMapLinkJSON.map((item) => (
+                  <a
+                    href={"https://" + item.link}
+                    key={item.codesRoadMapLinksId}
+                    rel="noreferrer"
+                    target="_blank"
+                  >
+                    Spreadsheet to download containing the roadmap
+                  </a>
+                ))}
+              </td>
             </tr>
             <tr>
               <td>
                 <b>Where to find out more</b>
                 <p> ** </p>
-                <p>If you want to know more about the roadmap purpose and process, <a href={wikiPage} target="_blank">visit the Wiki page here.</a></p>
-                <p>The roadmap launched in March 2023 and it will evolve over time – we welcome your views on how we can develop it further for you.  If you have comments or feedback on any aspect of the roadmap, please contact <a href="#" onClick={() => window.location = 'mailto:enquiries@recmanager.co.uk'}>enquiries@recmanager.co.uk</a>.</p>
+                <p>
+                  If you want to know more about the roadmap purpose and
+                  process,{" "}
+                  <a href={wikiPage} target="_blank" rel="noreferrer">
+                    visit the Wiki page here.
+                  </a>
+                </p>
+                <p>
+                  The roadmap launched in March 2023 and it will evolve over
+                  time – we welcome your views on how we can develop it further
+                  for you.  If you have comments or feedback on any aspect of
+                  the roadmap, please contact{" "}
+                  <a
+                    href="#"
+                    onClick={() =>
+                      (window.location = "mailto:enquiries@recmanager.co.uk")
+                    }
+                  >
+                    enquiries@recmanager.co.uk
+                  </a>
+                  .
+                </p>
               </td>
             </tr>
           </tbody>
@@ -57,7 +100,7 @@ export async function getServerSideProps({ req, res }) {
   //Passing the data to the code-road-map page via props
   return {
     props: {
-      latestCodesRoadMapLinkJSON
-    }
+      latestCodesRoadMapLinkJSON,
+    },
   };
 }
