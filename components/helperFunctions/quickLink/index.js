779180e03ccf57a53c23c87be6f9d83fb2265193
Parnell <dec.parnell@capgemini.com>
2023-04-26 09:31:03 +0100

    adding comments

--- components/helperFunctions/quickLink/index.js
+++ components/helperFunctions/quickLink/index.js
@@ -2,6 +2,10 @@ import styles from "../../../styles/quickLink.module.css";
 import Link from "next/link";
 
 function QuickLink(props) {
+  //props
+  //title = text to display at teh top of the link (string)
+  //link = where to redirect to
+  //image = react icons object to display under the text
   return (
     <Link href={props.link}>
       <div className={` ${styles.quickLink} box`}>
