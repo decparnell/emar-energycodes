aab877358801f2d2006f87789af6e73f1593ea1f
Parnell <dec.parnell@capgemini.com>
2023-05-25 09:06:25 +0100

    adding body and action vars

--- pages/api/session.js
+++ pages/api/session.js
@@ -3,7 +3,8 @@
 // pages/api/session.js
 export default function handler(req, res) {
   //add page name
-  const { action } = req.body;
+  const body = req.body;
+  const action = body.action;
   fetch(
     `https://prod-12.uksouth.logic.azure.com/workflows/a01770cba8f44c8a90274a6faa24955d/triggers/manual/paths/invoke/email/${req.session.user.name_id}/action/${action}?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=vm5xuq9xqyj6xN0P_NBrRPjDsElEJhOsWIWcmjfdzak`
   )
