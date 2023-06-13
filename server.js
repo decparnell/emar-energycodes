67d5355e253fe2bc36a83b995caf9bbdef3d287a
Parnell <dec.parnell@capgemini.com>
2023-06-13 11:57:53 +0100

    moving api to server

--- server.js
+++ server.js
@@ -166,6 +166,22 @@ app.prepare().then(() => {
     });
   });
 
+  server.post("/api/session", (req, res) => {
+    //add page name
+    const { actionName } = req.body;
+    console.log(actionName);
+    fetch(
+      `https://prod-12.uksouth.logic.azure.com/workflows/a01770cba8f44c8a90274a6faa24955d/triggers/manual/paths/invoke/email/${req.session.user.name_id}/action/${actionName}?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=vm5xuq9xqyj6xN0P_NBrRPjDsElEJhOsWIWcmjfdzak`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching session data:", error);
+      });
+  });
+
   server.all("*", (req, res) => {
     if (req.session && typeof req.session.user !== "undefined") {
       return handle(req, res);
