515f14b4b60d617df683361d35e3f09281e74e20
Parnell <dec.parnell@capgemini.com>
2023-06-13 14:12:08 +0100

    adding body parser back in

--- server.js
+++ server.js
@@ -12,7 +12,14 @@ app.prepare().then(() => {
   var cookieParser = require("cookie-parser");
   //cookies-session
   var session = require("express-session");
-  server.use(express.json());
+  // If you're using express <4.0:
+  var bodyParser = require("body-parser");
+  server.use(
+    bodyParser.urlencoded({
+      extended: true,
+    })
+  );
+  //bodyParser.json()
 
   // creating 24 hours from milliseconds
   const oneDay = 1000 * 60 * 60 * 24;
