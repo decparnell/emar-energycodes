f18c1294e682b2cb94712215223827dd5e2d66ac
Anika Mehangra <anika.mehangra@capgemini.com>
2023-07-10 16:32:27 +0100

    Connected API successfully to nlp chatbox

--- pages/nlp/index.js
+++ pages/nlp/index.js
@@ -6,69 +6,57 @@ import QuestionBox from "../../components/nlp/questionBox";
 
 function NLP() {
   const [messageResponse, setMessageResonse] = useState([]);
-  const [botAnswer, setBotAnswer] = useState("");
 
-  const questionHandler = (event) => {
-    event.preventDefault();
-    fetchData();
+  const questionHandler = async (userQuestion) => {
 
-    const fetchData = async () => {
-      console.log(query);
-
-      const data = {
-        query: query,
-        api_params: {
-          engine: "gpt-35-turbo-0301",
-          temperature: 0.1,
-          max_tokens: 500,
-        },
-        logging: {
-          user_name: "user@example.com",
-          timestamp: 1234,
-        },
-      };
-
-      const bodyData = JSON.stringify(data);
-      const options = {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: bodyData,
-      };
-
-      fetch(
-        "https://recco-openai-qa.azurewebsites.net/api/answer_query?code=WVTZzRNJ3Hi2fH_tKF3hHiXJsirhpa8qQATso6LFTqIOAzFuFICWGQ==",
-        options
-      )
-        .then((response) => {
-          console.log(response);
-          if (!response.ok) {
-            throw new Error("Error occurred while fetching session data");
-          }
-          return response.json();
-        })
-        .then((data) => {
-          console.log("Session data:", data);
-          setBotAnswer(data.response.answer);
-        })
-        .then((userQuestion) => {
-          setMessageResonse((prevQuestionAnswer) => {
-            return [
-              ...prevQuestionAnswer,
-              {
-                question: userQuestion,
-                answer: botAnswer,
-                id: Math.round(Math.random() * 100).toString(),
-              },
-            ];
-          });
-          setBotAnswer("");
-        })
-        .catch((error) => {
-          console.error("Error fetching session data:", error);
-        });
+    const data = {
+      query: userQuestion,
+      api_params: {
+        engine: "gpt-35-turbo-0301",
+        temperature: 0.1,
+        max_tokens: 500,
+      },
+      logging: {
+        user_name: "user@example.com",
+        timestamp: 1234,
+      },
     };
+
+    const bodyData = JSON.stringify(data);
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: bodyData,
+    };
+
+    fetch(
+      "https://recco-openai-qa.azurewebsites.net/api/answer_query?code=WVTZzRNJ3Hi2fH_tKF3hHiXJsirhpa8qQATso6LFTqIOAzFuFICWGQ==",
+      options
+    )
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Error occurred while fetching session data");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        const botAnswer = data.response.answer;
+        setMessageResonse((prevQuestionAnswer) => {
+          return [
+            ...prevQuestionAnswer,
+            {
+              question: userQuestion,
+              answer: botAnswer,
+              id: Math.round(Math.random() * 100).toString(),
+            },
+          ];
+        });
+      })
+      .catch((error) => {
+        console.error("Error fetching session data:", error);
+      });
   };
 
   return (
