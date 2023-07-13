6c437bf90e81c2116244bbd5ed55a6290f00c74e
Parnell <dec.parnell@capgemini.com>
2023-07-13 18:44:06 +0100

    user actions logging

--- pages/api/session.js
+++ pages/api/session.js
@@ -5,9 +5,24 @@
 export default function handler(req, res) {
   //add page name
   const { actionName } = req.body;
-  const user = req.session.user.name_id;
+  const userName = req.session.user.name_id;
+  const data = {
+    user: userName,
+    action: actionName,
+  };
+
+  const bodyData = JSON.stringify(data);
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: bodyData,
+  };
+
   fetch(
-    `https://prod-12.uksouth.logic.azure.com/workflows/a01770cba8f44c8a90274a6faa24955d/triggers/manual/paths/invoke/email/${user}/action/${actionName}?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=vm5xuq9xqyj6xN0P_NBrRPjDsElEJhOsWIWcmjfdzak`
+    `https://prod-12.uksouth.logic.azure.com:443/workflows/a01770cba8f44c8a90274a6faa24955d/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=vm5xuq9xqyj6xN0P_NBrRPjDsElEJhOsWIWcmjfdzak`,
+    options
   )
     .then((response) => response.json())
     .then((data) => {
