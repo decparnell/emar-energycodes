f0d2218d07a50446dad9fd6a053ab96d79b86738
Parnell <dec.parnell@capgemini.com>
2023-07-18 19:34:18 +0100

    update aes

--- components/helperFunctions/AES/index.js
+++ components/helperFunctions/AES/index.js
@@ -1,11 +1,10 @@
 const CryptoJS = require("crypto-js");
-import { usePhrase } from "../../components/settings";
+const usePhrase = require("../../settings");
 
 const encryptWithAES = (text) => {
   return CryptoJS.AES.encrypt(text, usePhrase).toString();
 };
 const decryptWithAES = (ciphertext) => {
-  const passphrase = "123";
   const bytes = CryptoJS.AES.decrypt(ciphertext, usePhrase);
   const originalText = bytes.toString(CryptoJS.enc.Utf8);
   return originalText;
