e59288f94a162f85f874bdd95c61d8f73f99f729
Parnell <dec.parnell@capgemini.com>
2023-07-19 09:01:07 +0100

    adding string

--- components/moduleSettings.js
+++ components/moduleSettings.js
@@ -1,3 +1,3 @@
-const usePhrase = "oi45ionsdo23424vin4nrener5634346bfuihdf";
-const seshPhrase = "232124avunavo4123unvo23j0r8r88fhfb2389ru";
+const usePhrase = "ujhrfgnndfgjniufansdklnvkjngbarbnanevnnu";
+const seshPhrase = "wedfboaipweruibnapuihbnfpiuahnvpavnhpnvn";
 module.exports = { usePhrase, seshPhrase };
--- server.js
+++ server.js
@@ -171,7 +171,6 @@ app.prepare().then(() => {
   server.post("/api/session", jsonParser, (req, res) => {
     //add page name
     const { actionName } = req.body;
-    console.log("declan", AES.encryptWithAES("declan"));
     const userName = AES.encryptWithAES(req.session.user.name_id);
     const data = {
       user: userName,
