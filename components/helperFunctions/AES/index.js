c716030ef93e5ea4e8c89d8fddfc2e6027f5ff6e
Parnell <dec.parnell@capgemini.com>
2023-07-20 16:36:53 +0100

    AES changes

--- components/helperFunctions/AES/index.js
+++ components/helperFunctions/AES/index.js
@@ -2,13 +2,13 @@ var AES = require("crypto-js/aes");
 var ENC = require("crypto-js/enc-utf8");
 const moduleSettings = require("../../moduleSettings");
 
-export const encryptWithAES = (text) => {
+const encryptWithAES = (text) => {
   return AES.encrypt(text, moduleSettings.usePhrase).toString();
 };
-export const decryptWithAES = (ciphertext) => {
+const decryptWithAES = (ciphertext) => {
   const bytes = AES.decrypt(ciphertext, moduleSettings.usePhrase);
   const originalText = bytes.toString(ENC);
   return originalText;
 };
 
-//module.exports = { encryptWithAES, decryptWithAES };
+module.exports = { encryptWithAES, decryptWithAES };
