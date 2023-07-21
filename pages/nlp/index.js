634e056bcd55e14bc9c30285002984f2b304d16d
Parnell <dec.parnell@capgemini.com>
2023-07-21 14:24:04 +0100

    adding keys everywhere

--- pages/nlp/index.js
+++ pages/nlp/index.js
@@ -7,6 +7,7 @@ import UserQuestion from "../../components/nlp/userQuestion";
 import BotResponse from "../../components/nlp/botResponse";
 import QuestionHistory from "../../components/nlp/questionHistory";
 import QuestionHistoryItem from "../../components/nlp/questionHistoryItem";
+import { v4 as uuidv4 } from "uuid";
 
 function NLP() {
   const [botIsTyping, setBotIsTyping] = useState(false);
@@ -16,10 +17,8 @@ function NLP() {
   const [userQuestion, setUserQuestion] = useState("");
   const [previousQuestion, setPreviousQuestion] = useState("");
 
-  const fetchData = () => {
+  const fetchData = (queryId) => {
     if (isCurrentQuestion === true) {
-      //Storing queryid & user
-      const queryId = uuidv4();
       //Asked question data
       const data = {
         query: userQuestion,
@@ -28,8 +27,6 @@ function NLP() {
         uiVersion: uiVersion,
       };
     } else if (isCurrentQuestion === false) {
-      //Storing queryid & user
-      const queryId = uuidv4();
       //Previously asked question data
       const data = {
         query: previousQuestion,
@@ -55,6 +52,7 @@ function NLP() {
               ...prevChat,
               <BotResponse
                 messageValue={"An error occurred, please try again later"}
+                key={`${queryId}_BOT_ERROR`}
               />,
             ];
           });
@@ -68,7 +66,10 @@ function NLP() {
         const botAnswer = data.response.answer;
 
         setChatLog((prevChat) => {
-          return [...prevChat, <BotResponse messageValue={botAnswer} />];
+          return [
+            ...prevChat,
+            <BotResponse messageValue={botAnswer} key={`${queryId}_BOT`} />,
+          ];
         });
 
         setBotIsTyping(false);
@@ -80,12 +81,17 @@ function NLP() {
   };
 
   const questionHandler = async (userQuestion) => {
+    //Storing queryid & user
+    const queryId = uuidv4();
     setBotIsTyping(true);
     setIsCurrentQuestion(true);
     setUserQuestion(userQuestion);
 
     setChatLog((prevChat) => {
-      return [...prevChat, <UserQuestion messageValue={userQuestion} />];
+      return [
+        ...prevChat,
+        <UserQuestion messageValue={userQuestion} key={`${queryId}_USER`} />,
+      ];
     });
 
     setQuestionHistory((prevQuestion) => {
@@ -94,11 +100,12 @@ function NLP() {
         <QuestionHistoryItem
           messageValue={userQuestion}
           onAskQuestionFromHistory={questionHistoryItemHandler}
+          key={`${queryId}_PREV`}
         />,
       ];
     });
 
-    fetchData();
+    fetchData(queryId);
   };
 
   const questionHistoryItemHandler = (previousQuestion) => {
@@ -107,7 +114,10 @@ function NLP() {
     setPreviousQuestion(previousQuestion);
 
     setChatLog((prevChat) => {
-      return [...prevChat, <UserQuestion messageValue={previousQuestion} />];
+      return [
+        ...prevChat,
+        <UserQuestion messageValue={previousQuestion} key={123} />,
+      ];
     });
 
     fetchData();
