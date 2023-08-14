5e05728a815b649f3e0ef8dc1b5e9cc4a204669b
Parnell <dec.parnell@capgemini.com>
2023-08-14 10:15:12 +0100

    comment for test

--- pages/api/nlpSession.js
+++ pages/api/nlpSession.js
@@ -23,6 +23,7 @@ export default function handler(req, res) {
     },
     body: bodyData,
   };
+  //test
   return fetch(
     //"https://recco-openai-qa-prod.azurewebsites.net/api/answer_query?code=ioDCQKvxuvlEraJnocB1GQIIpK9dJI99kqsxDB8q4oUNAzFu3ussZw==",
     "https://recco-openai-qa.azurewebsites.net/api/answer_query?code=WVTZzRNJ3Hi2fH_tKF3hHiXJsirhpa8qQATso6LFTqIOAzFuFICWGQ==",
