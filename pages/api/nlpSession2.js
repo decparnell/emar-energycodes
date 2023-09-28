95eff0fb6aac5352a388b42ac736ba7442bb1891
Parnell <dec.parnell@capgemini.com>
2023-09-28 08:50:37 +0100

    pushing new api changes

--- pages/api/nlpSession2.js
+++ pages/api/nlpSession2.js
@@ -2,9 +2,13 @@ import { encryptWithAES } from "../../components/helperFunctions/AES";
 
 export default function handler(req, res) {
   // Get data submitted in request's body.
-  const { query } = req.body;
+  const { query, queryTimestamp, queryId, uiVersion } = req.body;
   const data = {
     query: query,
+    user_id: encryptWithAES(req.session.user.name_id),
+    query_timestamp: queryTimestamp,
+    query_id: queryId,
+    ui_version: uiVersion,
   };
   const bodyData = JSON.stringify(data);
   const options = {
@@ -17,6 +21,7 @@ export default function handler(req, res) {
   //test
   return fetch(
     //"https://recco-openai-qa-prod.azurewebsites.net/api/answer_query?code=ioDCQKvxuvlEraJnocB1GQIIpK9dJI99kqsxDB8q4oUNAzFu3ussZw==",
+    //"https://recco-openai-qa.azurewebsites.net/api/answer_query?code=WVTZzRNJ3Hi2fH_tKF3hHiXJsirhpa8qQATso6LFTqIOAzFuFICWGQ==",
     "https://recconlpcogsearch.azurewebsites.net/api/answer_query?code=psAPggtHpafMGciDC3Gjofz8lDzJbBmRdnXvUvO8g6AqAzFu3k2KOw==",
     options
   )
