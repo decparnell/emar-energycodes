69dc5705c3ae517b45acb2de8d99d688f3b5c975
Parnell <dec.parnell@capgemini.com>
2023-10-20 14:27:08 +0100

    fix for /erin useeffect

--- pages/codes-schedules/definitions/[busterm].js
+++ pages/codes-schedules/definitions/[busterm].js
@@ -1,6 +1,9 @@
 import { useRouter } from "next/router";
 import styles from "../../../styles/codes.module.css";
 import Head from "next/head";
+import { useEffect } from "react";
+import { LogUserInfo } from "../../../components/logging";
+
 function definitions({ versions, parts, sections, components, document }) {
   const docInfo = document[0];
   const partInfo = parts[0];
