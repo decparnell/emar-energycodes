01619eec06337aa884a7b2daf7471b65d173e5e3
Parnell <dec.parnell@capgemini.com>
2023-10-31 09:31:32 +0000

    updated email address to enquiries@recmanager.co.uk

--- components/nlp/botResponse_erin_develop.js
+++ components/nlp/botResponse_erin_develop.js
@@ -63,7 +63,7 @@ function BotResponse(props) {
 
   const sendSupportEmailHandler = () => {
     window.open(
-      "mailto:support@recmanager.co.uk?subject=Support query for ERIN"
+      "mailto:enquiries@recmanager.co.uk?subject=Support query for ERIN"
     );
   };
 
@@ -186,7 +186,7 @@ function BotResponse(props) {
       </a>
       , or by sending an email to{" "}
       <a className={`${styles.link}`} onClick={sendSupportEmailHandler}>
-        support@recmanager.co.uk
+        enquiries@recmanager.co.uk
       </a>
       . They'll be more than happy to help you!
     </>
