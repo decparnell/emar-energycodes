804cb46e382f49ddfd8484d7d377afd9beb695ea
Parnell <dec.parnell@capgemini.com>
2023-12-04 11:08:44 +0000

    defintions fix

--- components/tables/definitionsTables/index.js
+++ components/tables/definitionsTables/index.js
@@ -4,9 +4,9 @@ import AppContext from "../../context/AppContext";
 import { listItemsToIgnore } from "../../settings";
 import CreateCustomTag from "../../scheduleId/createCustomTag-scheduleId";
 import LinkTextFromDefinitions from "../../helperFunctions/linkTextFromDefinitions";
+import { format } from "date-fns";
 
 const DefinitionTables = (props) => {
-
   /* props: 
       definitions: set of definitions of a specific REC word
   */
@@ -14,8 +14,8 @@ const DefinitionTables = (props) => {
   const value = useContext(AppContext);
   let { selectedLetterFromNavBar } = value.state;
 
-  const letters = 'abcdefghijklmnopqrstuvwxyz';
-  const lettersArray = letters.split('');
+  const letters = "abcdefghijklmnopqrstuvwxyz";
+  const lettersArray = letters.split("");
   const rows = [];
   const columnsPerRow = 3; //Number of columns per Row
   for (let i = 0; i < lettersArray.length; i += columnsPerRow) {
@@ -28,19 +28,17 @@ const DefinitionTables = (props) => {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
-  const defaultSectionId = { sectionId: 2237, sectionName: "Definitions" }
+  const defaultSectionId = { sectionId: 2237, sectionName: "Definitions" };
 
   useEffect(() => {
-    if(selectedLetterFromNavBar !== ""){
+    if (selectedLetterFromNavBar !== "") {
       loadContentByLetter(selectedLetterFromNavBar);
     }
   }, [selectedLetterFromNavBar]);
 
-
-
   const fetchDefinitionsByLetter = async (letter) => {
     //getDefinitionsByLetter
-    let url = `https://prod-31.uksouth.logic.azure.com/workflows/87f5aaaab030422a86899a6ca24c1351/triggers/request/paths/invoke/documentId/2/version/4.1/sectionId/2237/letter/${letter}?api-version=2016-10-01&sp=%2Ftriggers%2Frequest%2Frun&sv=1.0&sig=eWSPnGwLkUOb6ZM2MLxynlkR1vR09rab9cfCJNeAefA`;
+    let url = `https://prod-31.uksouth.logic.azure.com/workflows/87f5aaaab030422a86899a6ca24c1351/triggers/request/paths/invoke/documentId/2/version/${props.version}/sectionId/2237/letter/${letter}?api-version=2016-10-01&sp=%2Ftriggers%2Frequest%2Frun&sv=1.0&sig=eWSPnGwLkUOb6ZM2MLxynlkR1vR09rab9cfCJNeAefA`;
     setError(null);
 
     try {
@@ -52,17 +50,26 @@ const DefinitionTables = (props) => {
     } catch (error) {
       setError(error);
     }
-  }
+  };
 
   const CreateAlphabeticTableContent = (props) => {
     let content = [];
 
-    const formatData = [{ components: data, indent: 0, partId_FK: 1029, sectionId: 2237, sectionName: "Definitions", sectionOrder: 3 }];
+    const formatData = [
+      {
+        components: data,
+        indent: 0,
+        partId_FK: 1029,
+        sectionId: 2237,
+        sectionName: "Definitions",
+        sectionOrder: 3,
+      },
+    ];
+    console.log(formatData);
 
     formatData.map((section) => {
-      let componentsInSection = section.components.filter(function (el2) {
-        return el2.sectionId_FK === section.sectionId;
-      });
+      console.log(section);
+      let componentsInSection = section.components;
 
       const clauses = componentsInSection.filter(function (el2) {
         return listItemsToIgnore.indexOf(el2.componentType) == -1;
@@ -97,11 +104,7 @@ const DefinitionTables = (props) => {
       }
 
       content.push(
-        <div
-          id={`sec${section.sectionId}`}
-          key={section.sectionName}
-          className={styles.tableDefinitions}
-        >
+        <div id={`sec${section.sectionId}`} className={styles.tableDefinitions}>
           <h3>
             ({section.sectionOrder})
             {LinkTextFromDefinitions(section.sectionName, definitions)}
@@ -112,16 +115,18 @@ const DefinitionTables = (props) => {
     });
 
     return content;
-
-  }
+  };
 
   const DefinitionsNotFound = () => {
     return (
       <div className={styles.infoLabelDefinitions}>
-        <p>Definitions are not found for the letter: <b>{selectedAlphabeticLetter}</b></p>
+        <p>
+          Definitions are not found for the letter:{" "}
+          <b>{selectedAlphabeticLetter}</b>
+        </p>
       </div>
     );
-  }
+  };
 
   const handleLetterClick = async (letter) => {
     value.setSelectedLetterFromNavBar(letter);
@@ -139,18 +144,34 @@ const DefinitionTables = (props) => {
 
   return (
     <>
-      <p className={styles.infoLabelDefinitions}>Definitions are organized alphabetically. Please select a letter.</p>
-      <table 
+      <p className={styles.infoLabelDefinitions}>
+        Definitions are organized alphabetically. Please select a letter.
+      </p>
+      <table
         className={styles.alphabetIndexTable}
-        id={selectedAlphabeticLetter === "" || data.length <= 2 ? `sec${defaultSectionId.sectionId}` : 'AlphabeticTableSearchId'}
-        key={selectedAlphabeticLetter === "" ? defaultSectionId.sectionName : 'AlphabeticTableSearchKey'}
+        id={
+          selectedAlphabeticLetter === "" || data.length <= 2
+            ? `sec${defaultSectionId.sectionId}`
+            : "AlphabeticTableSearchId"
+        }
+        key={
+          selectedAlphabeticLetter === ""
+            ? defaultSectionId.sectionName
+            : "AlphabeticTableSearchKey"
+        }
       >
         <tbody>
           {rows.map((row, rowIndex) => (
             <tr key={rowIndex}>
               {row.map((letter, columnIndex) => (
-                <td key={columnIndex} onClick={() => handleLetterClick(letter)}
-                  className={`${styles.cellHovered} ${selectedAlphabeticLetter === letter ? styles.cellSelected : ''}`}
+                <td
+                  key={columnIndex}
+                  onClick={() => handleLetterClick(letter)}
+                  className={`${styles.cellHovered} ${
+                    selectedAlphabeticLetter === letter
+                      ? styles.cellSelected
+                      : ""
+                  }`}
                 >
                   {"[" + letter + "]"}
                 </td>
@@ -161,14 +182,18 @@ const DefinitionTables = (props) => {
       </table>
       {
         //data.length > 2 because includes caption and header
-        isLoading ?
-          <p className={`${styles.loadingCustomStyle} loading-container`}>Loading</p>
-          : (selectedAlphabeticLetter !== "" && data.length > 2
-            ? <CreateAlphabeticTableContent /> : (selectedAlphabeticLetter !== "" ? <DefinitionsNotFound /> : null))
+        isLoading ? (
+          <p className={`${styles.loadingCustomStyle} loading-container`}>
+            Loading
+          </p>
+        ) : selectedAlphabeticLetter !== "" && data.length > 2 ? (
+          <CreateAlphabeticTableContent />
+        ) : selectedAlphabeticLetter !== "" ? (
+          <DefinitionsNotFound />
+        ) : null
       }
     </>
-  )
-
+  );
 };
 
 export default DefinitionTables;
